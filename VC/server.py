5cedfe47a2dab21915f1d27752105eda46c11d70
klemie <lemieuxkristopher@gmail.com>
2024-02-26 01:17:53 -0700

    websocket server

--- VC/server.py
+++ VC/server.py
@@ -1,27 +1,34 @@
-import socket 
-import threading
+import asyncio
+import websockets
 
-# local host
-HOST = '127.0.0.1'
-PORT = 9001 
+class WebSocketServer:
+    def __init__(self, host="localhost", port=8765):
+        self.host = host
+        self.port = port
+        self.server = None
+        self.server_continue = None
+        self.loop = asyncio.get_event_loop()
 
-def handle_client(client_socket):
-    while True:
-        data = client_socket.recv(1024)
-        if not data:
-            break
+    async def _handler(self, websocket, path):
+        async for message in websocket:
+            await self._handle_message(websocket, message)
+    
+    async def _handle_message(self, websocket, message):
+        message = message.strip()
+        match message.identifier:
+            case "instrumentation":
+                await self.send_message(websocket, "Received instrumentation message")
+            case "controls":
+                await self.send_message(websocket, "Received controls message")
+    
+    async def send_message(self, websocket, message):
+        await websocket.send(message)
+    
+    def start(self):
+        self.server = websockets.serve(self.handler, self.host, self.port)
+        asyncio.get_event_loop().run_until_complete(self.server)
+        asyncio.get_event_loop().run_forever()
 
-        print()
-        client_socket.sendall(data)
-    client_socket.close()
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-
-    server.bind((HOST, PORT))
-    server.listen()
-
-    while True:
-        connection, address = server.accept()
-        
-        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
-        client_thread.start()
+    async def stop(self):
+        self.server.close()
+        self.loop.run_until_complete(self.server.wait_closed())
