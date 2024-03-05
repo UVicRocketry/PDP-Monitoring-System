ecdbb05c0d1fc1bb85088976fe501bb6637281e1
klemie <lemieuxkristopher@gmail.com>
2024-03-05 12:28:11 -0800

    remove client and updated asyncio to .Feature()

--- VC/server.py
+++ VC/server.py
@@ -1,34 +1,28 @@
 import asyncio
 import websockets
+import json
 
 class WebSocketServer:
-    def __init__(self, host="localhost", port=8765):
+    def __init__(self, host="localhost", port=8888):
         self.host = host
         self.port = port
         self.server = None
         self.server_continue = None
-        self.loop = asyncio.get_event_loop()
 
     async def _handler(self, websocket, path):
         async for message in websocket:
             await self._handle_message(websocket, message)
     
     async def _handle_message(self, websocket, message):
-        message = message.strip()
-        match message.identifier:
-            case "instrumentation":
-                await self.send_message(websocket, "Received instrumentation message")
-            case "controls":
-                await self.send_message(websocket, "Received controls message")
+        message = json.loads(message)
+        print(message)
     
     async def send_message(self, websocket, message):
         await websocket.send(message)
     
-    def start(self):
-        self.server = websockets.serve(self.handler, self.host, self.port)
-        asyncio.get_event_loop().run_until_complete(self.server)
-        asyncio.get_event_loop().run_forever()
+    async def start(self):
+        async with websockets.serve(self._handler, self.host, self.port):
+            await asyncio.Future()
 
-    async def stop(self):
-        self.server.close()
-        self.loop.run_until_complete(self.server.wait_closed())
+socket = WebSocketServer()
+asyncio.run(socket.start())
