1cb0d7565701a7b5883f819035e15ba8ab89497b
klemie <lemieuxkristopher@gmail.com>
2024-04-09 03:38:36 -0700

    change to list comprehension for code longevity

--- VC/test/feature_completion.py
+++ VC/test/feature_completion.py
@@ -30,7 +30,6 @@ class TestFeatureCompletion():
             Computes the zeta index
         '''
         T = np.sum(self.__rhos * self.__taus)
-        print(T)
         return (((self.__rhos * self.__taus) / T)*100)
     
     @property
@@ -43,13 +42,10 @@ rhos: list[int] = None
 taus: list[int] = None
 zetas: list[float] = None
 
+TEST_CASE_NUMBERS = [3, 6]
 
-test_case_data = [
-    pd.read_csv("test-case-completion-matrix-1.3.csv"),
-    pd.read_csv("test-case-completion-matrix-1.6.csv")
-]
+test_case_data = [pd.read_csv(f"test-case-completion-matrix-1.{num}.csv") for num in TEST_CASE_NUMBERS]
 
-test_case_numbers = [3, 6]
 
 NUMBER_OF_TEST_CASES = len(test_case_data)
 
@@ -65,11 +61,10 @@ if __name__ == '__main__':
         taus = test_case_competition_matrix["Priority importance"]
 
         completion = TestFeatureCompletion(rhos, taus, ids).compute_feature_completion()
-        print(completion)
         updated_matrix = test_case_competition_matrix.assign(zeta=list(completion))
 
         print(updated_matrix)
 
-        updated_matrix.to_csv(f"test-case-completion-matrix-1.{test_case_numbers[i]}.csv", index=False)
+        updated_matrix.to_csv(f"test-case-completion-matrix-1.{TEST_CASE_NUMBERS[i]}.csv", index=False)
 
     
