37893dbd6642886871c4465070832044ac85da70
Kris Lemieux <45109638+klemie@users.noreply.github.com>
2024-09-26 19:38:26 -0700

    add termocouple & debugging

--- VC/instrumentation/read_labjack.py
+++ VC/instrumentation/read_labjack.py
@@ -11,21 +11,6 @@ X1000 = 0b00110000
 SING = 0b00000000
 DIFF = 0b10000000
 
-#Get starting reference temp of LabJack
-Tref = d.getTemperature() - 273.15
-
-c0 = -1.7600413686 * 10**-2
-c1 = 3.8921204975 * 10**-2
-c2 = 1.8558770032 * 10**-5
-c3 = -9.9457592874 * 10**-8
-c4 = 3.1840945719 * 10**-10
-c5 = -5.6072844889 * 10**-13
-c6 = 5.6075059059 * 10**-16
-c7 = -3.2020720003 * 10**-19
-c8 = 9.7151147152 * 10**-23
-c9 = -1.2104721275 * 10**-26
-
-voltage_reference = c0 + c1*Tref + c2*Tref**2 + c3*Tref**3 + c4*Tref**4 + c5*Tref**5 + c6*Tref**6 + c7*Tref**7 + c8*Tref**8 + c9*Tref**9
 
 '''
 NOTE:
@@ -161,7 +146,7 @@ channel_settings = [(86, DIFF | X1000), # L_RUN_TANK (SEEMS GOOD. CHECK CAL)
                     (55, SING | X100),  # T_COMB_CHMBR
                     (53, SING | X100),  # T_POST_COMB_CHMBR
                     (57, SING | X100),  # T_INJECTOR
-                    (49, SING | X1000)]  # T_RUN_TANK 
+                    (49, SING | X100)]  # T_RUN_TANK 
 
 # These gains and offsets convert raw volts to the SI unit of the sensor
 
@@ -199,6 +184,32 @@ CHAN_SHUNT        = 'AIN82' # Not implemented yet on cart
 
 # Set up the stream
 d = u6.U6()
+
+#Get starting reference temp of LabJack
+Tref = d.getTemperature() - 273.15
+
+c0 = -1.7600413686 * 10**-2
+c1 = 3.8921204975 * 10**-2
+c2 = 1.8558770032 * 10**-5
+c3 = -9.9457592874 * 10**-8
+c4 = 3.1840945719 * 10**-10
+c5 = -5.6072844889 * 10**-13
+c6 = 5.6075059059 * 10**-16
+c7 = -3.2020720003 * 10**-19
+c8 = 9.7151147152 * 10**-23
+c9 = -1.2104721275 * 10**-26
+
+voltage_reference = c0 + \
+                    c1*Tref + \
+                    c2*Tref**2 + \
+                    c3*Tref**3 + \
+                    c4*Tref**4 + \
+                    c5*Tref**5 + \
+                    c6*Tref**6 + \
+                    c7*Tref**7 + \
+                    c8*Tref**8 + \
+                    c9*Tref**9
+
 d.streamConfig(
         ScanFrequency   = scan_frequency,
         ChannelNumbers  = [x[0] for x in channel_settings],
@@ -214,10 +225,12 @@ if samples_per_packet < len(channel_settings):
              ") must be at least the number of channels: (" + \
              str(len(channel_settings)) + ")!")
 
+
+
 # Avoid having to power cycle the LJ on restart
 try:
     d.streamStop()
-except LowlevelErrorException as e:
+except Exception as e:
     print("Tried to stop stream that wasn't started producing error:\n")
     print(str(e))
 
@@ -228,6 +241,54 @@ except LabJackException as e:
     print("Failed to start LabJack")
     print(str(e))
     exit()
+
+# Function to turn thermocouple voltages to kelvin
+def v_to_K(voltage):
+    voltage_actual = voltage
+    if voltage > 0.020644 and voltage < 0.054886:
+        C0 = -131.8058
+        C1 = 48.30222
+        C2 = -1.646031 
+        C3 = 0.05464731
+        C4 = -0.0009650715
+        C5 = 0.000008802193
+        C6 = -0.00000003110810
+        C7 = 0
+        C8 = 0
+        C9 = 0
+    elif voltage > -0.005891 and voltage < 0:
+        C0 = 0
+        C1 = 25.173462
+        C2 = -1.1662878
+        C3 = -1.0833638
+        C4 = -0.89773540
+        C5 = -0.37342377
+        C6 = -0.086632643
+        C7 = -0.010450598
+        C8 = -0.00051920577
+        C9 = 0
+    elif voltage > 0 and voltage < 0.020644:
+        C0 = 0
+        C1 = 25.08355
+        C2 = 0.07860106
+        C3 = -0.2503131
+        C4 = 0.08315270
+        C5 = -0.01228034
+        C6 = 0.0009804036
+        C7 = -0.0000413030
+        C8 = 0.000001057734
+        C9 = -0.00000001052755
+    else:
+       return 0
+    
+    voltage_actual = voltage_actual*1000
+
+    tempC = C0 + \
+    C1*voltage_actual + C2*voltage_actual**2 + C3*voltage_actual**3 + \
+    C4*voltage_actual**4 + C5*voltage_actual**5 + C6*voltage_actual**6 + \
+    C7*voltage_actual**7 + C8*voltage_actual**8 + C9*voltage_actual**9
+   
+    return (tempC + 273.15)
 
 with open('instrumentation_data.txt', 'w') as file:
 
@@ -289,6 +350,8 @@ with open('instrumentation_data.txt', 'w') as file:
             converted['T_COMB_CHMBR'] = v_to_K(sum(T_COMB_CHMBR)/len(T_COMB_CHMBR))
             converted['T_POST_COMB'] = v_to_K(sum(T_POST_COMB)/len(T_POST_COMB))
 
+            print(f'v:{sum(T_RUN_TANK)/len(T_RUN_TANK)}, t: {v_to_K(sum(T_RUN_TANK)/len(T_RUN_TANK))}')
+
             # Write to file so websocket can send to ground support
             file.write(f'{json.dumps(converted)}\n')
 
@@ -296,58 +359,4 @@ with open('instrumentation_data.txt', 'w') as file:
               tmp.write(f'{json.dumps(converted)}')
               tmp.write('\n!')
 
-# Function to turn thermocouple voltages to kelvin
-def v_to_K(voltage):
-    voltage_actual = voltage + voltage_reference
-    if voltage > 0.020644 and voltage < 0.054886:
-        C0 = -131.8058
-        C1 = 48.30222
-        C2 = -1.646031 
-        C3 = 0.05464731
-        C4 = -0.0009650715
-        C5 = 0.000008802193
-        C6 = -0.00000003110810
-        C7 = 0
-        C8 = 0
-        C9 = 0
-    elif voltage > -0.005891 and voltage < 0:
-        C0 = 0
-        C1 = 25.173462
-        C2 = -1.1662878
-        C3 = -1.0833638
-        C4 = -0.89773540
-        C5 = -0.37342377
-        C6 = -0.086632643
-        C7 = -0.010450598
-        C8 = -0.00051920577
-        C9 = 0
-    elif voltage > 0 and voltage < 0.020644:
-        C0 = 0
-        C1 = 25.08355
-        C2 = 0.07860106
-        C3 = -0.2503131
-        C4 = 0.08315270
-        C5 = -0.01228034
-        C6 = 0.0009804036
-        C7 = -0.0000413030
-        C8 = 0.000001057734
-        C9 = -0.00000001052755
-    else:
-        C0 = 0
-        C1 = 0
-        C2 = 0
-        C3 = 0
-        C4 = 0
-        C5 = 0
-        C6 = 0
-        C7 = 0
-        C8 = 0
-        C9 = 0
-        print("Thermocouple voltage out of range   ", voltage_actual)
-    
-    tempC = C0 + \
-    C1*voltage_actual + C2*voltage_actual**2 + C3*voltage_actual**3 + \
-    C4*voltage_actual**4 + C5*voltage_actual**5 + C6*voltage_actual**6 + \
-    C7*voltage_actual**7 + C8*voltage_actual**8 + C9*voltage_actual**9
-   
-    return (tempC + 273.15)
+
