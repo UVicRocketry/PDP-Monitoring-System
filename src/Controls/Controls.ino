551bc572763445583da89a71d10afc2ccece0b19
Hayden Toupin <h.e.toupin@gmail.com>
2024-10-02 20:06:08 -0700

    Added Control Firmware

---
+++ src/Controls/Controls.ino
@@ -0,0 +1,368 @@
+/*  Valve Cart and Feed System: Controls - Valve Controls - Rework V 1.1
+   Author(s):
+   Matthew Ebert
+   Logan Sewell
+   JJ
+   Joshua Piuti
+   Organization: UVic Rocketry
+*/
+#include "Valve.h"
+
+//*********Limit Pins*********
+#define OpenLimitMEV 30
+#define CloseLimitMEV 27
+#define OpenLimitN2OV 41
+#define CloseLimitN2OV 40
+#define OpenLimitRTV 39
+#define CloseLimitRTV 38
+#define OpenLimitN2F 42
+#define CloseLimitN2F 43
+#define OpenLimitERV 34
+#define CloseLimitERV 35
+#define OpenLimitN2OF 33
+#define CloseLimitN2OF 32
+#define OpenLimitNCV 28
+#define CloseLimitNCV 29
+//****************************
+
+//******Enable Pins **********
+#define N2OFEnab 46
+#define N2OVEnab 47
+#define N2FEnab 49
+#define ERVEnab 50
+#define MEVEnab 51
+
+//*********Driver Pins********
+#define StepMEV 19
+#define DirMEV  20
+#define StepN2OV 4
+#define DirN2OV  3
+#define StepN2F 10
+#define DirN2F  9
+#define StepERV 15
+#define DirERV  14
+#define StepN2OF 17
+#define DirN2OF  16
+//****************************
+
+//***Solenoid & Igniter Pins***
+#define Igniter 45
+#define Solenoid 48
+#define RTVEnab 37
+//****************************
+
+//*********Safety Pins********
+#define KeySwitch 5
+#define EStop 6
+//****************************
+
+
+int IgniterState = 0;
+int TargetState [9];
+
+void MoveToTarget();
+void setTarget (String valveCommands);
+String readCSV(String * rxBuf);
+void sendState (void);
+
+bool ABORT = false;
+//Assining Valves
+//Stepper Valves
+Valve N2F = Valve(OpenLimitN2F, CloseLimitN2F, StepN2F, DirN2F, 20);
+Valve N2OV = Valve(OpenLimitN2OV, CloseLimitN2OV, StepN2OV, DirN2OV, 4);
+Valve N2OF  = Valve(OpenLimitN2OF, CloseLimitN2OF, StepN2OF, DirN2OF, 4);
+Valve ERV  = Valve(OpenLimitERV, CloseLimitERV, StepERV, DirERV, 2);
+Valve MEV  = Valve(OpenLimitMEV, CloseLimitMEV, StepMEV, DirMEV, 4);
+
+//Other Valves
+Valve NCV  = Valve(OpenLimitNCV, CloseLimitNCV);
+Valve RTV  = Valve(OpenLimitRTV, CloseLimitRTV, RTVEnab);
+
+void setup() {
+  // Sets Limit Pins
+  //enable set up
+  pinMode(N2OFEnab, OUTPUT);
+  pinMode(N2OVEnab, OUTPUT);
+  pinMode(N2FEnab, OUTPUT);
+  pinMode(ERVEnab, OUTPUT);
+  pinMode(MEVEnab, OUTPUT);
+
+  //Currently does not effect motors??
+  digitalWrite(N2OFEnab, HIGH);
+  digitalWrite(N2OVEnab, HIGH);
+  digitalWrite(N2FEnab, HIGH);
+  digitalWrite(ERVEnab, HIGH);
+  digitalWrite(MEVEnab, HIGH);
+
+  //Unsure how these operate
+  pinMode(Solenoid, OUTPUT);
+  digitalWrite(Solenoid, LOW);
+  pinMode(Igniter, OUTPUT);
+  digitalWrite(Igniter, LOW);
+  //********************
+
+  //Sets up EStop and Keyswitch
+  pinMode(EStop, INPUT_PULLUP);
+  pinMode(KeySwitch, INPUT_PULLUP);
+
+
+  //Serial_Startup
+  Serial.begin(115200);
+  //time in ms the serial blocks waiting for data
+  Serial.setTimeout(10);
+  //*********************************************
+
+  Serial.println("VC,STATUS,STARTUP,SUCCESS");
+  Serial.println("VC,CF,Initalizing_to_safe_state:(ALL Closed)");
+  sendState();
+  delay(1000);
+  //Set all target states to closed
+  for(int i = 0; i<9; i++)
+  {
+    TargetState[i] = 1;
+  }
+}
+
+
+void loop() {
+  
+  if (Serial.available() || digitalRead(EStop) == HIGH)
+  {//if a message has been recieved
+    //read all serial data
+    String rxBuffer = Serial.readString();
+    /*Should the buffer be cleared before entire message is sent/recieved
+    a fragment will be created and not parsed. An end flag should be checked
+    to ensure this case does not occur.*/
+    String Command = "";
+    String TYPE = "";
+    String SOURCE = "";
+    //read source of data
+    SOURCE = readCSV(&rxBuffer);
+    //read type of data
+    TYPE = readCSV(&rxBuffer);
+    //Send acknolodgement
+    //CCSerial.println("VC,ACK,");
+    /* Currently there is no error correcting or proper acknoledgement handling
+    This ACK is for debugging only. Proper protocols should be developed to ensure
+    a message is handled properly*/
+    if (TYPE == "UNABORT" && ABORT)
+    {
+      Serial.println("VC,STATUS,CANCELLED ABORT");
+      ABORT = false;
+    } else if (TYPE == "ABORT" || ABORT == true || digitalRead(EStop) == HIGH)
+    {
+      ABORT = true;
+      //Serial.println(TYPE);
+      Serial.println("VC,STATUS,ABORTED");
+      digitalWrite(Igniter, LOW);
+      digitalWrite(Solenoid, LOW);
+
+      //implement abort code
+      TargetState[0] = 1;  //Close N2OF
+      TargetState[1] = -1; //Open N2OV
+      TargetState[2] = 1;  //Close N2F
+      TargetState[3] = -1; //Open Relief Valve
+      TargetState[4] = 1;  //Close MEV
+      TargetState[5] = 1;  //Close RTV
+      TargetState[6] = 1;  //Close Igniter
+      TargetState[7] = 1;  //Close NCV
+      
+    } else if (TYPE == "CTRL")
+    {
+      //Serial.println(TYPE);
+      setTarget(rxBuffer);
+    } else if (TYPE == "CONNECT")
+    {
+      Serial.println("VC,STATUS,ESTABLISH");
+      sendState();
+    } else if (TYPE == "SUMMARY")
+    { 
+      //Serial.println(TYPE);
+      sendState();
+    } else
+    {
+      Serial.println("VC,ERROR,UNKNOWNCOMMAND," + SOURCE + "," + TYPE + "," + Command);
+    }
+  }
+
+  //called every loop to step motors if necessary
+  //disabled by keyswitch lockout on VC unless being aborted
+  if(digitalRead(KeySwitch) == LOW || ABORT)  {
+    MoveToTarget();
+  }
+}
+
+
+/***********FUNCTIONS*******************/
+
+
+void sendState (void)
+{
+  Serial.print("VC,SUMMARY");
+  Serial.print(",N2OF," +  N2OF.strState());
+  Serial.print(",N2OV," +  N2OV.strState());
+  Serial.print(",N2F," +  N2F.reverseStrState());
+  Serial.print(",RTV," +  RTV.solStrState());
+  Serial.print(",ERV," +  ERV.strState());
+  Serial.print(",MEV," +  MEV.strState());
+  Serial.print(",NCV," +  NCV.strState());
+  Serial.println("");
+}
+
+
+String readCSV (String * rxBuf)
+{
+  String Command = "";
+  char c;
+  c = (*rxBuf)[0];
+  while (c != '\n' && c !=  ',' && (*rxBuf).length() > 1) {
+    Command += c;
+    (*rxBuf).remove(0, 1);
+    c = (*rxBuf)[0];
+  }
+  (*rxBuf).remove(0, 1);
+  return Command;
+}
+
+
+void setTarget (String valveCommands)
+{
+  String Label = "";
+  String Value = "";
+  int index = 0;
+  while (valveCommands.length() > 1)
+  {
+    Label = readCSV(&valveCommands);
+    Value = readCSV(&valveCommands);
+
+
+    if (Label == "N2OF")
+    {
+    } else   if (Label ==  "N2OV")
+    {
+      index = 1;
+    } else if (Label ==  "N2F")
+    {
+      index = 2;
+    } else if (Label ==  "ERV")
+    {
+      index = 3;
+    } else if (Label ==  "MEV")
+    {
+      index = 4;
+    } else if (Label ==  "RTV")
+    {
+      index = 5;
+    } else if (Label ==  "IGPRIME")
+    {
+      index = 6;
+    } else if (Label == "NCV")
+    {
+      index = 7;
+    } else
+    {
+      index = 8;
+      Serial.println("VC,ERROR,UNKOWNVALVE");
+    }
+
+    if (Value == "OPEN")
+    {
+      TargetState[index] = -1;
+    }
+    else if (Value == "CLOSE")
+    {
+      TargetState[index] = 1;
+    } else
+    {
+      TargetState[index] = 0;
+    }
+
+  }
+}
+
+
+
+
+
+void MoveToTarget()
+{
+
+  if ( N2OF.state() != TargetState[0] && TargetState[0]!=0)
+  {    
+    N2OF.moveStep(TargetState[0]);
+  }
+  if ( N2OF.getChange())
+  {
+    sendState();
+  }
+
+  if ( N2OV.state() != TargetState[1] && TargetState[1]!=0)
+  {
+   N2OV.moveStep(-TargetState[1]);
+  }
+  if ( N2OV.getChange())
+  {
+    sendState();
+  }
+
+  if ( N2F.state() != TargetState[2] && TargetState[2]!=0)
+  {
+    N2F.moveStep(TargetState[2]);
+  }
+  if ( N2F.getChange())
+  {
+    sendState();
+  }
+
+  if ( ERV.state() != TargetState[3] && TargetState[3]!=0)
+  {
+    ERV.moveStep(-TargetState[3]);
+  }
+  if ( ERV.getChange())
+  {
+    sendState();
+  }
+
+  if ( MEV.state() != TargetState[4] && TargetState[4]!=0)
+  {
+    MEV.moveStep(-TargetState[4]);
+  }
+  if ( MEV.getChange())
+  {
+    sendState();
+  }
+
+
+  if (RTV.solState() != TargetState[5] && TargetState[5]!=0)
+  {
+    RTV.moveSol(TargetState[5]);   
+    sendState();     
+  }
+  
+  if (IgniterState != TargetState[6] && TargetState[6]!=0)
+  {
+    if (TargetState[6] == 1)
+    {
+      digitalWrite(Igniter, LOW);
+      IgniterState = 1;
+    } else if (TargetState[6] == -1) {
+      digitalWrite(Igniter, HIGH);
+      IgniterState = -1;
+    }
+  }
+
+  if (NCV.state() != TargetState[7] && TargetState[7]!=0)
+  {
+    if (TargetState[7] == 1)
+    {
+      digitalWrite(Solenoid, LOW);
+    } else if (TargetState[7] == -1)
+    {
+      digitalWrite(Solenoid, HIGH);
+    }
+  }
+  if (NCV.getChange())
+  {
+    sendState();
+  }
+}
---
+++ src/Controls/Valve.cpp
@@ -0,0 +1,181 @@
+#include "Arduino.h"
+#include "Valve.h"
+
+// Constructor
+Valve::Valve(int OpenLimit, int CloseLimit) {
+  this->OpenLimit = OpenLimit;
+  this->CloseLimit = CloseLimit;
+  this->change = 0;
+  this->prevState = 0;//!digitalRead(OpenLimit) | !digitalRead(CloseLimit);
+  pinMode(OpenLimit, INPUT_PULLUP);
+  pinMode(CloseLimit, INPUT_PULLUP);
+}
+
+Valve::Valve(int OpenLimit, int CloseLimit, int StepPin, int DirPin, int StepSpeed) {
+  this->OpenLimit = OpenLimit;
+  this->CloseLimit = CloseLimit;
+  this->StepPin = StepPin;
+  this->DirPin = DirPin;
+  this->StepSpeed = StepSpeed;
+  this->change = 0;
+  this->prevState = 0;//!digitalRead(OpenLimit) | !digitalRead(CloseLimit);
+  pinMode(OpenLimit, INPUT_PULLUP);
+  pinMode(CloseLimit, INPUT_PULLUP);
+  pinMode(DirPin, OUTPUT);
+  pinMode(StepPin, OUTPUT);
+  StepTime = millis();
+}
+
+Valve::Valve(int OpenLimit, int CloseLimit, int SolEnab) {
+  this->OpenLimit = OpenLimit;
+  this->CloseLimit = CloseLimit;
+  this->SolEnab = SolEnab;
+  this->change = 0;
+  this->activated = 1;
+  this->prevState = 0;//!digitalRead(OpenLimit) | !digitalRead(CloseLimit);
+  pinMode(OpenLimit, INPUT_PULLUP);
+  pinMode(CloseLimit, INPUT_PULLUP);
+  pinMode(SolEnab, OUTPUT);
+  digitalWrite(SolEnab, LOW);  
+}
+
+//*********State functions*********
+int Valve::state() {
+  if (digitalRead(OpenLimit) == LOW) {
+    change = (prevState != -1);
+    prevState = -1;
+    return -1;
+  } else if (digitalRead(CloseLimit) == LOW) {
+    change = (prevState != 1);
+    prevState = 1;
+    return 1;
+  } else {
+    change = (prevState != 0);
+    prevState = 0;
+    return 0;
+  }
+}
+
+int Valve::solState() {
+  if(activated == 1) {
+    change = (prevState != 1);
+    prevState = 1;
+    return 1;
+  } else if (activated == -1) {
+    change = (prevState != -1);
+    prevState = -1;
+    return -1;
+  } else {
+    change = (prevState != 0);
+    prevState = 0;
+    return 0;
+  }
+}
+
+String Valve::strState() {
+  if (digitalRead(OpenLimit) == LOW) {
+    change = (prevState != -1);
+    prevState = -1;
+    return "OPEN";
+  } else if (digitalRead(CloseLimit) == LOW) {
+    change = (prevState != 1);
+    prevState = 1;
+    return "CLOSE";
+  } else {
+    change = (prevState != 0);
+    prevState = 0;
+    return "TRANSIT";
+  }
+}
+
+String Valve::reverseStrState() {
+  if (digitalRead(OpenLimit) == LOW) {
+    change = (prevState != -1);
+    prevState = -1;
+    return "CLOSE";
+  } else if (digitalRead(CloseLimit) == LOW) {
+    change = (prevState != 1);
+    prevState = 1;
+    return "OPEN";
+  } else {
+    change = (prevState != 0);
+    prevState = 0;
+    return "TRANSIT";
+  }
+}
+
+String Valve::solStrState() {
+  if(activated == 1) {
+    change = (prevState != 1);
+    prevState = 1;
+    return "VENTING";
+  } else if (activated == -1) {
+    change = (prevState != -1);
+    prevState = -1;
+    return "FILLING";
+  } else {
+    change = (prevState != 0);
+    prevState = 0;
+    return "TRANSIT";    
+  }
+}
+
+
+bool Valve::moveStep(int Dir)
+{ 
+  //Serial.print("Stepping Pin:");
+  //Serial.println(StepPin);
+  if ((millis() - StepTime) > StepSpeed)
+  {
+    //Serial.println(millis());
+    //Serial.println(StepTime);
+    //Serial.println(millis()-StepTime);
+     if(Dir==1)
+    {
+       digitalWrite(DirPin, 1);
+    }else
+    {
+      digitalWrite(DirPin, 0);
+    }
+    delayMicroseconds(50);
+    digitalWrite(StepPin, 1);
+    delayMicroseconds(50);
+    digitalWrite(StepPin, 0);
+    StepTime=millis();
+    return true;
+    
+  }else if(StepTime>millis())
+  {
+    //overflow of 32bit millis number
+    StepTime=millis();
+  }
+  return false;
+}
+
+bool Valve::moveSol(int State)
+{
+  if (State == 1)
+  {
+    digitalWrite(SolEnab, 0);
+    activated = 1;
+  } else
+  {
+    digitalWrite(SolEnab, 1);
+    activated = -1;
+  }
+  return true;
+}
+
+//*********Getters*********
+int Valve::getOpenLimit() {
+  return this->OpenLimit;
+}
+
+int Valve::getCloseLimit() {
+  return this->CloseLimit;
+}
+
+
+int Valve::getChange() {
+  return change;
+}
