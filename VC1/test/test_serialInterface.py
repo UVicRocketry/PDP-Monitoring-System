323bc762f64e847134f6f6b7a24699502762c8f1
Hayden Toupin <h.e.toupin@gmail.com>
2024-10-03 18:27:26 -0700

    Renamed VC to src

--- VC1/test/test_serialInterface.py
+++
@@ -1,6 +0,0 @@
-import unittest
-from  serial import Serial
-
-class TestSerialInterface(unittest.TestCase):
-    def test():
-        pass
--- VC1/test/test_ws_server.py
+++
@@ -1,5 +0,0 @@
-import unittest
-
-class TestWSS(unittest.TestCase):
-    def test():
-        pass
