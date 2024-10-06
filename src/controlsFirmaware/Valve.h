a31979797b6021a0491d2078ad4445a5ddabeab1
hToupin <118093901+hToupin@users.noreply.github.com>
2024-10-06 11:30:38 -0700

    Added Control Firmware (#31)
    * Added Control Firmware
    
    * Renamed VC to src
    
    * Renamed VC to src
    
    ---------
    
    Co-authored-by: klemie <lemieuxkristopher@gmail.com>

---
+++ src/controlsFirmaware/Valve.h
@@ -0,0 +1,38 @@
+#ifndef MY_VALVE_H
+#define MY_VALVE_H
+#include <Arduino.h>
+class Valve {
+ 
+    private:
+    int OpenLimit;
+    int CloseLimit;
+    int SolEnab;
+    int change;
+    int prevState;
+    uint32_t StepTime;
+    int StepPin;
+    int DirPin;
+    int StepSpeed;
+    int activated;
+
+    public:
+    // Constructor
+    Valve(int OpenLimit, int CloseLimit);
+    Valve(int OpenLimit, int CloseLimit, int StepPin, int DirPin, int StepSpeed);
+    Valve(int OpenLimit, int CloseLimit, int SolEnab);
+
+    //State functions
+    int state();
+    int solState();
+    String strState();
+    String solStrState();
+    String reverseStrState();
+    bool moveStep(int Dir);
+    bool moveSol(int State);
+
+    // Getters
+    int getOpenLimit();
+    int getCloseLimit();
+    int getChange();
+};
+#endif 
